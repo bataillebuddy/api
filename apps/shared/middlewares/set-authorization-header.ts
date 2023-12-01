eca9c19b95f87ddbe37b7e8603af19fdc3fcd9b9
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-12-01 17:26:01 +0100

    feat(middlewares): register middlewares for `auth`

---
+++ apps/shared/middlewares/set-authorization-header.ts
@@ -0,0 +1,13 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class SetAuthorizationHeader {
+  public async handle({ request }: HttpContextContract, next: () => Promise<void>) {
+    const token = request.cookie('token')
+
+    if (token) {
+      request.headers().authorization = `Bearer ${token}`
+    }
+
+    await next()
+  }
+}
--- start/kernel.ts
+++ start/kernel.ts
@@ -20,7 +20,11 @@ import Server from '@ioc:Adonis/Core/Server'
 | are defined for every HTTP requests.
 |
 */
-Server.middleware.register([() => import('@ioc:Adonis/Core/BodyParser')])
+Server.middleware.register([
+  () => import('@ioc:Adonis/Core/BodyParser'),
+  () => import('App/shared/middlewares/set-authorization-header'),
+  () => import('App/shared/middlewares/silent-auth'),
+])
 
 /*
 |--------------------------------------------------------------------------
@@ -38,4 +42,6 @@ Server.middleware.register([() => import('@ioc:Adonis/Core/BodyParser')])
 | Route.get('dashboard', 'UserController.dashboard').middleware('auth')
 |
 */
-Server.middleware.registerNamed({})
+Server.middleware.registerNamed({
+  auth: () => import('App/shared/middlewares/auth'),
+})
