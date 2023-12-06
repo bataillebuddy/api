a00a487e3d82a79e25c26210bcaba2815b3ff451
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-12-06 21:31:43 +0100

    feat(auth): add register method

--- apps/authentication/controllers/authentication_controller.ts
+++ apps/authentication/controllers/authentication_controller.ts
@@ -1,7 +1,10 @@
 import { type HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'Domains/users/models/user'
+import { CreateUserValidator } from 'App/authentication/validators/authentication_validator'
+import AuthenticationService from 'App/authentication/services/authentication_service'
 
 export default class AuthenticationController {
+  private authenticationService = AuthenticationService
   public async login({ request, response, auth }: HttpContextContract) {
     const email = request.input('email')
     const password = request.input('password')
@@ -16,6 +19,19 @@ export default class AuthenticationController {
     } catch (error) {
       return response.unauthorized('Invalid credentials')
     }
+  }
+
+  public async register({ request, response }: HttpContextContract) {
+    const data = await request.validate(CreateUserValidator)
+
+    const user = await this.authenticationService.createUser(data)
+
+    return response.send({
+      message: 'A user has just been created',
+      user
+    })
+
+
   }
 
   public async me({ auth, response }: HttpContextContract) {
--- apps/authentication/routes.ts
+++ apps/authentication/routes.ts
@@ -3,6 +3,7 @@ import Route from '@ioc:Adonis/Core/Route'
 export default () => {
   Route.group(() => {
     Route.post('/login', 'authentication_controller.login').as('authentication.login')
+    Route.post('/register', 'authentication_controller.register').as('authentication.register')
 
     Route.group(() => {
       Route.get('/me', 'authentication_controller.me').as('authentication.me')
