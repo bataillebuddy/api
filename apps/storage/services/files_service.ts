e4a4ed4f79cf06dc9b5e72b3def322b451117501
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-12-07 01:02:07 +0100

    feat(storage): create files service

---
+++ apps/storage/services/files_service.ts
@@ -0,0 +1,29 @@
+import { type MultipartFileContract } from '@ioc:Adonis/Core/BodyParser'
+import Logger from '@ioc:Adonis/Core/Logger'
+import { err } from 'pino-std-serializers'
+import Drive from '@ioc:Adonis/Core/Drive'
+
+class FilesServices {
+  public async createFile(file: MultipartFileContract, location: string, name: string) {
+    try {
+      await file.moveToDisk(location, {
+        name: name,
+      }, 's3')
+    } catch (err) {
+      Logger.error(err.stack())
+    }
+  }
+
+  public async deleteFile(key: string) {
+    try {
+      await Drive.getStats(key)
+
+      await Drive.delete(key)
+    } catch (error) {
+      // Exception for file not found in S3
+      throw error
+    }
+  }
+}
+
+export default new FilesServices()
